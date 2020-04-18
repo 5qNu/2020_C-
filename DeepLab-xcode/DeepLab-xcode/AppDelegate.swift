a300a52e3193a4dbb79f69313bb14a370b2919d2
5qNu <116646492+5qNu@users.noreply.github.com>
2020-04-18 08:43:06 +0000

    add file upload

--- DeepLab-xcode/DeepLab-xcode/AppDelegate.swift
+++ DeepLab-xcode/DeepLab-xcode/AppDelegate.swift
@@ -13,4 +13,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions:
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplic
