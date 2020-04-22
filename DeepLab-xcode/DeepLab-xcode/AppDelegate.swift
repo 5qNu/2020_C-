8ce76b6a398b5f0c2b91906094f6f022c1e5f9c9
5qNu <116646492+5qNu@users.noreply.github.com>
2020-04-22 11:19:33 +0000

    build config

--- DeepLab-xcode/DeepLab-xcode/AppDelegate.swift
+++ DeepLab-xcode/DeepLab-xcode/AppDelegate.swift
@@ -14,4 +14,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for 
+        // Override point for customization
