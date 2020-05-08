97a48089ff3a3e7cc537fa0d95a076772a3bd38c
5qNu <116646492+5qNu@users.noreply.github.com>
2020-05-08 15:29:09 +0000

    update id

--- DeepLab-xcode/DeepLab-xcode/AppDelegate.swift
+++ DeepLab-xcode/DeepLab-xcode/AppDelegate.swift
@@ -20,4 +20,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     // MARK: UISceneSession Lifecycle
 
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.Connectio
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UI
