574e2a38237a622ed4d97b7fbf46547490817b47
5qNu <116646492+5qNu@users.noreply.github.com>
2020-05-25 21:09:30 +0000

    Removed empty lines.

--- DeepLab-xcode/DeepLab-xcode/AppDelegate.swift
+++ DeepLab-xcode/DeepLab-xcode/AppDelegate.swift
@@ -26,4 +26,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
-    func applicatio
+    func application(_ application
