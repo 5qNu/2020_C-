2b731c5f9e3e65b14e8fe7bdd657cd4ad29eeb8d
5qNu <116646492+5qNu@users.noreply.github.com>
2020-05-30 11:11:00 +0000

    isdn/gigaset: fix CAPI disconnect B3 handling

--- DeepLab-xcode/DeepLab-xcode/AppDelegate.swift
+++ DeepLab-xcode/DeepLab-xcode/AppDelegate.swift
@@ -26,4 +26,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
-    func application(_ application: UIApplication, didDiscardScene
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessi
