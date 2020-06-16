1e7b8a8f7b400aca530745ea34bfdd7eeacc4efa
5qNu <116646492+5qNu@users.noreply.github.com>
2020-06-16 14:05:00 +0000

    Update requests from 2.18.4 to 2.19.1

--- DeepLab-xcode/DeepLab-xcode/AppDelegate.swift
+++ DeepLab-xcode/DeepLab-xcode/AppDelegate.swift
@@ -28,4 +28,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
-        // If any sessions w
+        // If any sessions were discar
