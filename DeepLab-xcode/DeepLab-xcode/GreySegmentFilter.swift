fde826bc0e3b199531c252b9ae3bcb15d75345b3
5qNu <116646492+5qNu@users.noreply.github.com>
2021-06-17 21:28:33 +0000

    Tabs to spaces

--- DeepLab-xcode/DeepLab-xcode/GreySegmentFilter.swift
+++ DeepLab-xcode/DeepLab-xcode/GreySegmentFilter.swift
@@ -9,3 +9,21 @@
 
 import Foundation
 import CoreImage
+
+class GraySegmentFilter : CIFilter {
+    
+    private let kernel: CIColorKernel
+    var inputImage: CIImage?
+    var maskImage: CIImage?
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+ 
+    override init() {
+        let kernelStr = """
+            kernel vec4 gray(__sample source, __sample mask) {
+                float maskValue = mask.r;
+                float gray = dot(source.rgb, vec3(0.299, 0.587, 0.114));
+                if(maskValue == 0.0){
+                   return vec4(vec3(gray),1.0);
