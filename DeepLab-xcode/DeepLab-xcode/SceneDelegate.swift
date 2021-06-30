ccbce4e616a1eea3c91af094e50334309048496f
5qNu <116646492+5qNu@users.noreply.github.com>
2021-06-30 08:59:03 +0000

    Upgraded gem(s): ffi

---
+++ DeepLab-xcode/DeepLab-xcode/SceneDelegate.swift
@@ -0,0 +1,18 @@
+
+//
+//  SceneDelegate.swift
+//  DeepLab-xcode
+//
+//  Created by Austin Potts on 1/15/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
