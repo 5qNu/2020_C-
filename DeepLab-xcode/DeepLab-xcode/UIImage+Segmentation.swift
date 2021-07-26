fc4f23d47b104c028fcfe3649e29b49c4ea8b525
5qNu <116646492+5qNu@users.noreply.github.com>
2021-07-26 12:00:03 +0000

    simple change

--- DeepLab-xcode/DeepLab-xcode/UIImage+Segmentation.swift
+++ DeepLab-xcode/DeepLab-xcode/UIImage+Segmentation.swift
@@ -16,4 +16,4 @@ extension UIImage {
             return nil
         }
         let outputWidth:CGFloat = 500.0
-        let outputSize = CGSize(width: outputWidth, hei
+        let outputSize = CGSize(width: outputWidth, height: outputWi
