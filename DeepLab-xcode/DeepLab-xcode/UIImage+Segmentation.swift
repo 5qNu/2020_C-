f81e0f763c74264b16a75bdedfe3c924899b6798
5qNu <116646492+5qNu@users.noreply.github.com>
2021-07-29 21:45:53 +0000

    old code removed

--- DeepLab-xcode/DeepLab-xcode/UIImage+Segmentation.swift
+++ DeepLab-xcode/DeepLab-xcode/UIImage+Segmentation.swift
@@ -17,4 +17,4 @@ extension UIImage {
         }
         let outputWidth:CGFloat = 500.0
         let outputSize = CGSize(width: outputWidth, height: outputWidth * (self.size.height / self.size.width))
-        let resizeImg = UIImage(cgImage: cgImage
+        let resizeImg = UIImage(cgImage: cgImage).resize(siz
