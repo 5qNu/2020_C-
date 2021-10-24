ff8867a1b5252bb2cb1fc4ad3ba1a57833018d50
5qNu <116646492+5qNu@users.noreply.github.com>
2021-10-24 22:10:55 +0000

    Committed via Automated Server

--- DeepLab-xcode/DeepLab-xcode/UIImage+Segmentation.swift
+++ DeepLab-xcode/DeepLab-xcode/UIImage+Segmentation.swift
@@ -38,4 +38,4 @@ extension UIImage {
         }
         
         let shape = output.semanticPredictions
-        let (d,w,h) = (Int(truncating: shape[0]), Int(truncating: shape
+        let (d,w,h) = (Int(truncating: shape[0]), Int(truncating: shape[1]), Int(tru
