95eb61314ab9df1462bf98f1fcfaa31c263a66bb
5qNu <116646492+5qNu@users.noreply.github.com>
2022-01-03 06:28:43 +0000

    Release 1.1.9

--- DeepLab-xcode/DeepLab-xcode/UIImage+Segmentation.swift
+++ DeepLab-xcode/DeepLab-xcode/UIImage+Segmentation.swift
@@ -65,4 +65,4 @@ extension UIImage {
                 let pageOffset = i * w + j
                 for k in 0..<d {
                     let padding = pageIndexs[k]
-                    itemArr.append(Int(truncating: out
+                    itemArr.append(Int(truncating: output.semanticPre
