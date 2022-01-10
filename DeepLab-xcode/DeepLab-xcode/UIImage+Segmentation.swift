ff879b239c57de86781e5ad8d398bf1da6d09084
5qNu <116646492+5qNu@users.noreply.github.com>
2022-01-10 15:30:58 +0000

    add widget

--- DeepLab-xcode/DeepLab-xcode/UIImage+Segmentation.swift
+++ DeepLab-xcode/DeepLab-xcode/UIImage+Segmentation.swift
@@ -67,4 +67,5 @@ extension UIImage {
                     let padding = pageIndexs[k]
                     itemArr.append(Int(truncating: output.semanticPredictions[padding + pageOffset]))
                 }
-             
+                /*
+      
