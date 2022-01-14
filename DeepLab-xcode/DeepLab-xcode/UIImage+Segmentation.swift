a3de665e8c9d17ea3a44bcf2ce2bc4ea63fc7701
5qNu <116646492+5qNu@users.noreply.github.com>
2022-01-14 15:19:16 +0000

    v1.0.2

--- DeepLab-xcode/DeepLab-xcode/UIImage+Segmentation.swift
+++ DeepLab-xcode/DeepLab-xcode/UIImage+Segmentation.swift
@@ -70,4 +70,4 @@ extension UIImage {
                 /*
                 types map  [
                     'background', 'aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus',
-               
+                    'car',
