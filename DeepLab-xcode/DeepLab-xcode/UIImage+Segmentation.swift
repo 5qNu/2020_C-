4d356accae82396ddd2510c70fb6b6ca5d6d2eec
5qNu <116646492+5qNu@users.noreply.github.com>
2022-01-21 07:04:31 +0000

    l10n: French update

--- DeepLab-xcode/DeepLab-xcode/UIImage+Segmentation.swift
+++ DeepLab-xcode/DeepLab-xcode/UIImage+Segmentation.swift
@@ -70,4 +70,5 @@ extension UIImage {
                 /*
                 types map  [
                     'background', 'aeroplane', 'bicycle', 'bird', 'boat', 'bottle', 'bus',
-                    'car', 'cat', 'chair', 'cow', 'diningtable', 'dog', 'horse', 'motor
+                    'car', 'cat', 'chair', 'cow', 'diningtable', 'dog', 'horse', 'motorbike',
+     
