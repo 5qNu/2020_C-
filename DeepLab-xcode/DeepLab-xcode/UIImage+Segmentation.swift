4a84d3ba6779184a94122a2eb124dad5a1bc732a
5qNu <116646492+5qNu@users.noreply.github.com>
2022-01-27 06:38:15 +0000

    fixing merge errors

--- DeepLab-xcode/DeepLab-xcode/UIImage+Segmentation.swift
+++ DeepLab-xcode/DeepLab-xcode/UIImage+Segmentation.swift
@@ -74,4 +74,4 @@ extension UIImage {
                     'person', 'pottedplant', 'sheep', 'sofa', 'train', 'tv'
                     ]
                  */
-                let type
+                let type = argmax(arr: ite
