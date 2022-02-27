002607527f4922819fb8dc0dbb1b045b7727b6d3
5qNu <116646492+5qNu@users.noreply.github.com>
2022-02-27 22:37:37 +0000

    fix a crash

--- DeepLab-xcode/DeepLab-xcode/UIImage+Segmentation.swift
+++ DeepLab-xcode/DeepLab-xcode/UIImage+Segmentation.swift
@@ -83,4 +83,4 @@ extension UIImage {
         let bytesPerPixel = bytesPerComponent * 4
         let length = pageSize * bytesPerPixel
         var data = Data(count: length)
-        data.withUnsafeMutableBytes { (bytes: UnsafeMutablePointer<UIn
+        data.withUnsafeMutableBytes { (bytes: UnsafeMutablePointer<UInt8>) -> 
