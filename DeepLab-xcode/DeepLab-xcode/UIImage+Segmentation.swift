4d733183c28a1c1f08caf835aeb9384f96bf360a
5qNu <116646492+5qNu@users.noreply.github.com>
2022-03-24 11:53:15 +0000

    writing docs

--- DeepLab-xcode/DeepLab-xcode/UIImage+Segmentation.swift
+++ DeepLab-xcode/DeepLab-xcode/UIImage+Segmentation.swift
@@ -91,4 +91,4 @@ extension UIImage {
             let reserve = [8,12,15]
             for pix in res{
                 let v:UInt8 = reserve.contains(pix) ? 255 : 0
-            
+                for _ in 0
