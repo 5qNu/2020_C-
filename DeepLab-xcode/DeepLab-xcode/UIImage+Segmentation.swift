f8e310040a1bf44461673a049413a4906f04c4ea
5qNu <116646492+5qNu@users.noreply.github.com>
2022-08-22 10:15:15 +0000

    CR fix

--- DeepLab-xcode/DeepLab-xcode/UIImage+Segmentation.swift
+++ DeepLab-xcode/DeepLab-xcode/UIImage+Segmentation.swift
@@ -136,4 +136,4 @@ extension UIImage {
                                      attrs as CFDictionary,
                                      &maybePixelBuffer)
 
-    guard status == kC
+    guard status == kCVReturnSucce
