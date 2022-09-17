515ef9c9aa3e77a6a8ae950bfe71cffbc9229307
5qNu <116646492+5qNu@users.noreply.github.com>
2022-09-17 19:19:02 +0000

    Font size

--- DeepLab-xcode/DeepLab-xcode/UIImage+Segmentation.swift
+++ DeepLab-xcode/DeepLab-xcode/UIImage+Segmentation.swift
@@ -141,4 +141,4 @@ extension UIImage {
     }
 
     CVPixelBufferLockBaseAddress(pixelBuffer, CVPixelBufferLockFlags(rawValue: 0))
-    let pixelData = CVPixelBufferGetBaseAddres
+    let pixelData = CVPixelBufferGetBaseAddress(pixelBuffer
