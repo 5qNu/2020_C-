05a638e559a2da4f7c6181dff4f0ef369e29ed8a
5qNu <116646492+5qNu@users.noreply.github.com>
2023-01-12 23:01:49 +0000

    Update backend.md

--- DeepLab-xcode/DeepLab-xcode/UIImage+Segmentation.swift
+++ DeepLab-xcode/DeepLab-xcode/UIImage+Segmentation.swift
@@ -192,4 +192,5 @@ fileprivate class SmoothFilter : CIFilter {
             kernel vec4 myColor(__sample source) {
                 float maskValue = smoothstep(0.3, 0.5, source.r);
                 return vec4(maskValue,maskValue,maskValue,1.0);
-   
+            }
+        """
