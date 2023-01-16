f3bc7baee7b38ca66db8774c1a94a12223d6aabf
5qNu <116646492+5qNu@users.noreply.github.com>
2023-01-16 22:49:08 +0000

    Update wechat sharing

--- DeepLab-xcode/DeepLab-xcode/UIImage+Segmentation.swift
+++ DeepLab-xcode/DeepLab-xcode/UIImage+Segmentation.swift
@@ -194,4 +194,5 @@ fileprivate class SmoothFilter : CIFilter {
                 return vec4(maskValue,maskValue,maskValue,1.0);
             }
         """
-        let kernels = CIColorKernel.makeKernels(source:kern
+        let kernels = CIColorKernel.makeKernels(source:kernelStr)!
+      
