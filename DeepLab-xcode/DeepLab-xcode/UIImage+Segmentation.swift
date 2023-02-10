b9d83dadc81cabcb75860b41932035c477d1f493
5qNu <116646492+5qNu@users.noreply.github.com>
2023-02-10 21:38:04 +0000

    26771

--- DeepLab-xcode/DeepLab-xcode/UIImage+Segmentation.swift
+++ DeepLab-xcode/DeepLab-xcode/UIImage+Segmentation.swift
@@ -201,4 +201,4 @@ fileprivate class SmoothFilter : CIFilter {
     
     override var outputImage: CIImage? {
         guard let inputImage = inputImage else {return nil}
-        let blurFilter = CIFilter.init(name:
+        let blurFilter = CIFilter.init(name: "CIGaussian
